b99ec9dafee469213cd13518a57afc1ce3acd6d6
Thomas Jaggi <thomas.jaggi@unic.com>
2018-04-24 18:01:07 +0200

    added vue-template-compiler to handle single file components

--- src/adapter.js
+++ src/adapter.js
@@ -5,6 +5,45 @@ const VueServerRenderer = require('vue-server-renderer');
 const fs = require('fs');
 const Adapter = require('@frctl/fractal').Adapter;
 const PathPlugin = require('./plugins/PathPlugin');
+const compiler = require('vue-template-compiler');
+const babel = require('babel-core');
+const requireFromString = require('require-from-string');
+const merge = require('lodash.merge');
+
+function parseComponent(content = '', path = '') {
+	// Parse file content
+	const component = compiler.parseComponent(content);
+
+	// Not a single file component
+	if (!component.template) {
+		return {
+			template: content,
+			script: {}
+		}
+	}
+
+	// Extract template
+	const template = component.template.content;
+
+	// Transpile ES6 to consumable script
+	const scriptCode = babel.transform(component.script.content, {
+		presets: [
+			['env', {
+				targets: {
+					node: 'current'
+				}
+			}]
+		]
+	}).code;
+
+	// Compile script
+	const script = requireFromString(scriptCode, '').default;
+
+	return {
+		script,
+		template
+	};
+}
 
 class VueAdapter extends Adapter {
 	constructor(source, app, config) {
@@ -20,11 +59,13 @@ class VueAdapter extends Adapter {
 			const props = component.configData ? Object.keys(component.configData.context) : [];
 
 			// Register all fractal components as Vue components
-			fs.readFileAsync(component.viewPath, 'utf8').then(template => {
-				Vue.component(component.name, {
-					template,
+			fs.readFileAsync(component.viewPath, 'utf8').then(content => {
+				const parsedComponent = parseComponent(content, component.viewPath);
+
+				Vue.component(component.name, merge({
+					template: parsedComponent.template,
 					props,
-				});
+				}, parsedComponent.script));
 			});
 		});
 
@@ -39,12 +80,13 @@ class VueAdapter extends Adapter {
 		meta = meta || {};
 
 		const renderer = VueServerRenderer.createRenderer();
+		const parsedComponent = parseComponent(str, path);
 
 		const config = this._app.config();
 
-		const vue = new Vue({
+		const vue = new Vue(merge({
 			data: context,
-			template: str,
+			template: parsedComponent.template,
 			computed: {
 				_self() {
 					return meta.self;
@@ -56,7 +98,7 @@ class VueAdapter extends Adapter {
 					return config;
 				}
 			}
-		});
+		}, parsedComponent.script));
 
 		return renderer.renderToString(vue).then(html => {
 			// Return the html without the empty comments used by Vue (v-if usage)
@@ -70,14 +112,17 @@ class VueAdapter extends Adapter {
 	updateVueComponent(view) {
 		const component = this._source.find(view.handle);
 
+		// TODO: Figure out how to pass path. view.handle?
+		const parsedComponent = compiler.parseComponent(component.content);
+
 		// Auto define props based on the keys used in the config
 		const props = component.configData ? Object.keys(component.configData.context) : [];
 
 		// Update vue component
-		Vue.component(component.name, {
-			template: component.content,
+		Vue.component(component.name, merge({
+			template: parsedComponent.template,
 			props
-		});
+		}, parsedComponent.script));
 	}
 }
 
